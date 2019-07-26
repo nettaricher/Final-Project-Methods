b9b2aca3f30d598ec5eec94333ff76e6914c1d73
Sagi Granot <sagig@ad.checkpoint.com>
2019-07-26 18:04:26 +0300

    adding checklist and checkbox

---
+++ Common/CheckBox.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Control.h"
+
+class CheckBox : public Control
+{
+private:
+	string value;
+	bool isSelected;
+
+public:	
+	CheckBox(short _left, short _top, string _value);
+	void draw(Graphics& GUI, int x, int y, size_t z);
+	void mousePressed(int x, int y, bool isLeft) ;
+	void keyDown(int keyCode, char charecter) {};
+	bool canGetFocus() { return TRUE; };
+	void flipBgToWhite();
+	void flipBgToBlack();
+	bool getIsSelected();
+	void setIsSelected();
+	~CheckBox();
+};
+
--- FinalProject/Source.cpp
+++ FinalProject/Source.cpp
@@ -1,11 +1,12 @@
 #include "../Common/Graphics.h"
 #include "../Common/EventEngine.h"
 #include "../Common/Button.h"
+#include "../Common/CheckList.h"
 #include <iostream>
 
 int main(int argc, char** argv)
 {
 	EventEngine e;
-	Button b(new NormalBorder(), 2, 23, "button");
-	e.run(b);
+	CheckList cl(new DblBorder(), 2, 2, { "check Box 1" , "check Box 2"  , "check Box 3" ,  "check Box 4" });
+	e.run(cl);
 }
