7ef436680506093da4d35582df386c9f1e483555
Netta Richer <nettaricher1@gmail.com>
2019-07-26 13:43:24 +0300

    add Button.h

---
+++ Controls/Button.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "../Common/Label.h"
+
+class Button : public Control
+{
+private:
+    string value;
+    int width, height;
+    bool isClicked;
+    vector<Control *> events;
+
+public:
+    Button(Border *border, short left, short top, string value);
+    void draw(Graphics &g, int x, int y, size_t z);
+    void mousePressed(int x, int y, bool isLeft);
+    bool canGetFocus() { return FALSE; };
+    int getWidth();
+    void setEvent(Control *c);
+    ~Button();
+};
