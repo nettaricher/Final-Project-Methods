a074ddb3ca111fcdddd8dff7cda66d69f4a4b6a1
Netta Richer <nettaricher1@gmail.com>
2019-07-26 19:00:35 +0300

    panel with checklist, button and textbox

--- FinalProject/Source.cpp
+++ FinalProject/Source.cpp
@@ -9,19 +9,22 @@
 int main(int argc, char **argv)
 {
 	EventEngine e;
-	// Button b(new NormalBorder(), 2, 23, "button");
 	Panel p(new NormalBorder(), 1, 1);
 	p.setWidth(100);
 	p.setHeight(30);
-	TextBox textBox1(new NormalBorder(), 2, 20, 20, 4);
-	TextBox textBox2(new NormalBorder(), 23, 20, 20, 4);
 
+	TextBox textBox1(new NormalBorder(), 2, 20, 20, 4);
+	Button b1(new DblBorder(), 2, 23, "Click me");
+	CheckList cl(new NormalBorder(), 2, 2, {"check Box 1", "check Box 2", "check Box 3", "check Box 4"});
+
+	p.add(&cl);
 	p.add(&textBox1);
+	p.add(&b1);
 
 	Control *firstFocus;
 	if (firstFocus = p.getFirstControl())
 	{
 		Control::setFocus(*firstFocus);
 	}
-	e.run(textBox1);
+	e.run(p);
 }
