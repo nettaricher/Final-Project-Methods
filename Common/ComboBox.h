bc3958fb48e4f5331def9f94f155af297c4c2ccb
noaEylon <37585299+NoaAylon@users.noreply.github.com>
2019-07-27 15:58:20 +0300

    add ComboBox header file

--- Common/ComboBox.h
+++ Common/ComboBox.h
@@ -3,20 +3,20 @@
 class ComboBox : public Control
 {
 private:
-    int currentOption;
-    int selectedOption;
-    int width;
-    bool isComboBoxOpen;
-    string title;
-    vector<string> listOptions;
+	int currentOption;
+	int selectedOption;
+	int width;
+	bool isComboBoxOpen;
+	string title;
+	vector<string> listOptions;
 
 public:
-    ComboBox(Border *border, int left, int top, vector<string> listOptions);
-    int getWidth() { return width; };
-    void mousePressed(int l, int t, bool isLeft);
-    void draw(Graphics &GUI, int x, int y, size_t z);
-    void keyDown(int keyCode, char charecter, Graphics &g);
-    bool canGetFocus() { return TRUE; };
-    bool isListObj() { return TRUE; }
-    ~ComboBox();
+	ComboBox(Border *border, int left, int top, vector<string> listOptions);
+	void mousePressed(int l, int t, bool isLeft);
+	int getWidth() { return width; };
+	void draw(Graphics &GUI, int x, int y, size_t z);
+	void keyDown(int keyCode, char charecter, Graphics &g);
+	bool canGetFocus() { return TRUE; };
+	bool isListObj() { return TRUE; }
+	~ComboBox();
 };
