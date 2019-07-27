bfe17e99dcc05795bbed3200578368505b8859d5
noaEylon <37585299+NoaAylon@users.noreply.github.com>
2019-07-27 18:53:15 +0300

    Add mouse click event for checkbox control

--- Common/CheckList.cpp
+++ Common/CheckList.cpp
@@ -2,222 +2,223 @@
 #include <iostream>
 using namespace std;
 
-
 //Constructor
-CheckList::CheckList(Border * _border, short _left, short _top, vector<string> _values) :Control(_left, _top, _border), currentCboxIndex(-1) , selectedIndex(0)
+CheckList::CheckList(Border *_border, short _left, short _top, vector<string> _values) : Control(_left, _top, _border), currentCboxIndex(-1), selectedIndex(0)
 {
-	for (int i = 0; i < _values.size(); i++) {
-		checkBoxs.push_back(new CheckBox(_left + 2, 1 + (i * 3), _values[i]));
-		COORD pos = { _left + (short)4  , _top + (short)(2 + (i * 3)) };
-		checkBoxCoord.push_back(pos);
+	for (int i = 0; i < _values.size(); i++)
+	{
+		checkBoxList.push_back(new CheckBox(_left + 2, top + (1 + (i * 3)), _values[i]));
 	}
 }
 
 //Overloads the father mathod draw()
-void CheckList::draw(Graphics & GUI, int x, int y, size_t z)
+void CheckList::draw(Graphics &g, int x, int y, size_t z)
 {
-	GUI.setBackground(bgColor);
-	GUI.setForeground(fgColor);
-	border->draw(GUI, x, y, 25, static_cast<int>(checkBoxs.size() * 4));
-	for (auto c : checkBoxs) {
-		c->draw(GUI,  c->getLeft()  , y  + c->getTop(), z);
+	g.setBackground(bgColor);
+	g.setForeground(fgColor);
+	border->draw(g, x, y, 25, static_cast<int>(checkBoxList.size() * 4));
+	for (auto c : checkBoxList)
+	{
+		c->draw(g, c->getLeft(), c->getTop(), z);
 	}
 }
 
 //reaction to mouse press
-void CheckList::mousePressed(int x, int y, bool isLeft)
+void CheckList::mousePressed(int posX, int posY, bool isLeft)
 {
+	for (auto checkBox : checkBoxList)
+	{
+		checkBox->mousePressed(posX, posY, isLeft);
+	}
 }
 
 //reaction to key down press
-void CheckList::keyDown(int keyCode, char charecter , Graphics & g)
+void CheckList::keyDown(int keyCode, char charecter, Graphics &g)
 {
-	int vectorSize = static_cast<int>(checkBoxs.size());
-	
+	int vectorSize = static_cast<int>(checkBoxList.size());
+
 	switch (keyCode)
 	{
-		//tab was pressed
-		case VK_TAB:
-			// if no focused
-			if (++currentCboxIndex == 0)
-			{
-				selectedIndex = currentCboxIndex;
-				checkBoxs[currentCboxIndex]->flipBgToWhite();
-			}
-			// if is in last position befor out of the check list
-			else if (currentCboxIndex == vectorSize -1)
-			{
-				checkBoxs[currentCboxIndex - 1]->flipBgToBlack();
-				checkBoxs[currentCboxIndex]->flipBgToWhite();
-				selectedIndex = currentCboxIndex;
-			}	
-			// middle
-			else
-			{
-				checkBoxs[currentCboxIndex - 1]->flipBgToBlack();
-				checkBoxs[currentCboxIndex]->flipBgToWhite();
-				selectedIndex = currentCboxIndex;
-			}
-			break;
+	//tab was pressed
+	case VK_TAB:
+		// if no focused
+		if (++currentCboxIndex == 0)
+		{
+			selectedIndex = currentCboxIndex;
+			checkBoxList[currentCboxIndex]->flipBgToWhite();
+		}
+		// if is in last position befor out of the check list
+		else if (currentCboxIndex == vectorSize - 1)
+		{
+			checkBoxList[currentCboxIndex - 1]->flipBgToBlack();
+			checkBoxList[currentCboxIndex]->flipBgToWhite();
+			selectedIndex = currentCboxIndex;
+		}
+		// middle
+		else
+		{
+			checkBoxList[currentCboxIndex - 1]->flipBgToBlack();
+			checkBoxList[currentCboxIndex]->flipBgToWhite();
+			selectedIndex = currentCboxIndex;
+		}
+		break;
 
-			//key down was pressed
-		case VK_DOWN:
-			// if no focused
-			if (++currentCboxIndex == 1)
-			{
-				checkBoxs[currentCboxIndex - 1]->flipBgToBlack();
-				checkBoxs[currentCboxIndex]->flipBgToWhite();
-				selectedIndex = currentCboxIndex;
-			}
-			else if (currentCboxIndex == 0)
-			{
-				checkBoxs[currentCboxIndex]->flipBgToWhite();
-				selectedIndex = currentCboxIndex;
-			}
-			// if is in last position befor out of the check list
-			else if (currentCboxIndex == vectorSize)
-			{
-				checkBoxs[currentCboxIndex - 1]->flipBgToBlack();
-				checkBoxs[0]->flipBgToWhite();
-				currentCboxIndex = 0;
-				selectedIndex = currentCboxIndex;
-			}
-			// middle
-			else
-			{
-				checkBoxs[currentCboxIndex - 1]->flipBgToBlack();
-				checkBoxs[currentCboxIndex]->flipBgToWhite();
-				selectedIndex = currentCboxIndex;
-			}
-			break;
+		//key down was pressed
+	case VK_DOWN:
+		// if no focused
+		if (++currentCboxIndex == 1)
+		{
+			checkBoxList[currentCboxIndex - 1]->flipBgToBlack();
+			checkBoxList[currentCboxIndex]->flipBgToWhite();
+			selectedIndex = currentCboxIndex;
+		}
+		else if (currentCboxIndex == 0)
+		{
+			checkBoxList[currentCboxIndex]->flipBgToWhite();
+			selectedIndex = currentCboxIndex;
+		}
+		// if is in last position befor out of the check list
+		else if (currentCboxIndex == vectorSize)
+		{
+			checkBoxList[currentCboxIndex - 1]->flipBgToBlack();
+			checkBoxList[0]->flipBgToWhite();
+			currentCboxIndex = 0;
+			selectedIndex = currentCboxIndex;
+		}
+		// middle
+		else
+		{
+			checkBoxList[currentCboxIndex - 1]->flipBgToBlack();
+			checkBoxList[currentCboxIndex]->flipBgToWhite();
+			selectedIndex = currentCboxIndex;
+		}
+		break;
 
-			//numpad2 was pressed like key down
-		case VK_NUMPAD2:
-			// if no focused
-			if (++currentCboxIndex == 1)
-			{
-				checkBoxs[currentCboxIndex - 1]->flipBgToBlack();
-				checkBoxs[currentCboxIndex]->flipBgToWhite();
-				selectedIndex = currentCboxIndex;
-			}
-			else if (currentCboxIndex == 0)
-			{
-				checkBoxs[currentCboxIndex]->flipBgToWhite();
-				selectedIndex = currentCboxIndex;
-			}
-			// if is in last position befor out of the check list
-			else if (currentCboxIndex == vectorSize)
-			{
-				checkBoxs[currentCboxIndex - 1]->flipBgToBlack();
-				checkBoxs[0]->flipBgToWhite();
-				currentCboxIndex = 0;
-				selectedIndex = currentCboxIndex;
-			}
-			// middle
-			else
-			{
-				checkBoxs[currentCboxIndex - 1]->flipBgToBlack();
-				checkBoxs[currentCboxIndex]->flipBgToWhite();
-				selectedIndex = currentCboxIndex;
-			}
-			break;
+		//numpad2 was pressed like key down
+	case VK_NUMPAD2:
+		// if no focused
+		if (++currentCboxIndex == 1)
+		{
+			checkBoxList[currentCboxIndex - 1]->flipBgToBlack();
+			checkBoxList[currentCboxIndex]->flipBgToWhite();
+			selectedIndex = currentCboxIndex;
+		}
+		else if (currentCboxIndex == 0)
+		{
+			checkBoxList[currentCboxIndex]->flipBgToWhite();
+			selectedIndex = currentCboxIndex;
+		}
+		// if is in last position befor out of the check list
+		else if (currentCboxIndex == vectorSize)
+		{
+			checkBoxList[currentCboxIndex - 1]->flipBgToBlack();
+			checkBoxList[0]->flipBgToWhite();
+			currentCboxIndex = 0;
+			selectedIndex = currentCboxIndex;
+		}
+		// middle
+		else
+		{
+			checkBoxList[currentCboxIndex - 1]->flipBgToBlack();
+			checkBoxList[currentCboxIndex]->flipBgToWhite();
+			selectedIndex = currentCboxIndex;
+		}
+		break;
 
-			//key up was pressed
-		case VK_UP:
-			// if no focused
-			if (--currentCboxIndex == 0)
-			{
-				checkBoxs[currentCboxIndex + 1]->flipBgToBlack();
-				checkBoxs[currentCboxIndex]->flipBgToWhite();
-				selectedIndex = currentCboxIndex;
-			}
-			else if (currentCboxIndex <= -1)
-			{
-				currentCboxIndex = vectorSize - 1;
-				checkBoxs[0]->flipBgToBlack();
-				checkBoxs[currentCboxIndex]->flipBgToWhite();
-				selectedIndex = currentCboxIndex;
-			}
-			// if is in last position befor out of the check list
-			// middle
-			else
-			{
-				checkBoxs[currentCboxIndex]->flipBgToWhite();
-				checkBoxs[currentCboxIndex + 1]->flipBgToBlack();	
-				selectedIndex = currentCboxIndex;
-			}
-			break;
+		//key up was pressed
+	case VK_UP:
+		// if no focused
+		if (--currentCboxIndex == 0)
+		{
+			checkBoxList[currentCboxIndex + 1]->flipBgToBlack();
+			checkBoxList[currentCboxIndex]->flipBgToWhite();
+			selectedIndex = currentCboxIndex;
+		}
+		else if (currentCboxIndex <= -1)
+		{
+			currentCboxIndex = vectorSize - 1;
+			checkBoxList[0]->flipBgToBlack();
+			checkBoxList[currentCboxIndex]->flipBgToWhite();
+			selectedIndex = currentCboxIndex;
+		}
+		// if is in last position befor out of the check list
+		// middle
+		else
+		{
+			checkBoxList[currentCboxIndex]->flipBgToWhite();
+			checkBoxList[currentCboxIndex + 1]->flipBgToBlack();
+			selectedIndex = currentCboxIndex;
+		}
+		break;
 
-			//numpad 8 was pressed like key up
-		case VK_NUMPAD8:
-			// if no focused
-			if (--currentCboxIndex == 0)
-			{
-				checkBoxs[currentCboxIndex + 1]->flipBgToBlack();
-				checkBoxs[currentCboxIndex]->flipBgToWhite();
-				selectedIndex = currentCboxIndex;
-			}
-			else if (currentCboxIndex <= -1)
-			{
-				currentCboxIndex = vectorSize - 1;
-				checkBoxs[0]->flipBgToBlack();
-				checkBoxs[currentCboxIndex]->flipBgToWhite();
-				selectedIndex = currentCboxIndex;
-			}
-			// if is in last position befor out of the check list
-			// middle
-			else
-			{
-				checkBoxs[currentCboxIndex]->flipBgToWhite();
-				checkBoxs[currentCboxIndex + 1]->flipBgToBlack();
-				selectedIndex = currentCboxIndex;
-			}
-			break;
+		//numpad 8 was pressed like key up
+	case VK_NUMPAD8:
+		// if no focused
+		if (--currentCboxIndex == 0)
+		{
+			checkBoxList[currentCboxIndex + 1]->flipBgToBlack();
+			checkBoxList[currentCboxIndex]->flipBgToWhite();
+			selectedIndex = currentCboxIndex;
+		}
+		else if (currentCboxIndex <= -1)
+		{
+			currentCboxIndex = vectorSize - 1;
+			checkBoxList[0]->flipBgToBlack();
+			checkBoxList[currentCboxIndex]->flipBgToWhite();
+			selectedIndex = currentCboxIndex;
+		}
+		// if is in last position befor out of the check list
+		// middle
+		else
+		{
+			checkBoxList[currentCboxIndex]->flipBgToWhite();
+			checkBoxList[currentCboxIndex + 1]->flipBgToBlack();
+			selectedIndex = currentCboxIndex;
+		}
+		break;
 
-			//space button was pressed to select current item and check it
-		case VK_SPACE:
+		//space button was pressed to select current item and check it
+	case VK_SPACE:
 
-			checkBoxs[selectedIndex]->setIsSelected();
-			break;
-
+		checkBoxList[selectedIndex]->setIsSelected();
+		break;
 	}
 }
 
 //return true if the checklist focus is on last child
 bool CheckList::getIsInLastChild()
-{	
-	int vectorSize = static_cast<int>(checkBoxs.size());
+{
+	int vectorSize = static_cast<int>(checkBoxList.size());
 	// if no focused
 	if (++currentCboxIndex == 0)
 	{
 		selectedIndex = currentCboxIndex;
-		checkBoxs[currentCboxIndex]->flipBgToWhite();
+		checkBoxList[currentCboxIndex]->flipBgToWhite();
 		return FALSE;
 	}
 	// if is in last position befor out of the check list
 	else if (currentCboxIndex == vectorSize - 1)
 	{
-		checkBoxs[currentCboxIndex]->flipBgToWhite();
-		checkBoxs[currentCboxIndex - 1]->flipBgToBlack();
+		checkBoxList[currentCboxIndex]->flipBgToWhite();
+		checkBoxList[currentCboxIndex - 1]->flipBgToBlack();
 		currentCboxIndex = vectorSize - 1;
 		selectedIndex = currentCboxIndex;
 		return FALSE;
 	}
-	else if (currentCboxIndex == vectorSize )
+	else if (currentCboxIndex == vectorSize)
 	{
-		checkBoxs[vectorSize - 1]->flipBgToBlack();
-		currentCboxIndex = - 1;
+		checkBoxList[vectorSize - 1]->flipBgToBlack();
+		currentCboxIndex = -1;
 		return TRUE;
 	}
 	// middle
 	else
 	{
-		checkBoxs[currentCboxIndex - 1]->flipBgToBlack();
-		checkBoxs[currentCboxIndex]->flipBgToWhite();
+		checkBoxList[currentCboxIndex - 1]->flipBgToBlack();
+		checkBoxList[currentCboxIndex]->flipBgToWhite();
 		selectedIndex = currentCboxIndex;
 		return FALSE;
 	}
-
 }
 
 //set focus method
@@ -225,7 +226,6 @@ void CheckList::onFocus(bool flag)
 {
 	focus = flag;
 }
-
 
 //Destructor
 CheckList::~CheckList()
