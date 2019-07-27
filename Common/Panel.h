47a561c000b0dc1338e1816f3612a1d711b45e54
Tomer L <tomerl101@gmail.com>
2019-07-27 16:05:45 +0300

    update Panel header file

--- Common/Panel.h
+++ Common/Panel.h
@@ -8,20 +8,18 @@ private:
 	short height;
 
 public:
-	Panel(Border* border, short _left, short _top);
+	Panel(Border* border, short left, short top);
 
 	void draw(Graphics& GUI, int x, int y, size_t z);
-	void mousePressed(int x, int y, bool isLeft);
+	void mousePressed(int posX, int posY, bool isLeft);
 	void keyDown(int keyCode, char charecter) {};
 	bool canGetFocus() { return TRUE; };
-	void add(Control* _control);
-	
+	void add(Control* control);
+	bool isInsideBoundaries(int posX, int posY, Control control);
 	Control* getFirstControl();
-
 	void getAllControls(vector<Control*> *controls);
 
-	void setWidth(short _width);
-	void setHeight(short _hige);
+	void setWidth(short width);
+	void setHeight(short hige);
 	~Panel();
 };
-
